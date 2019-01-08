85ca989c37c42ad8f5b5f747b4a4f6bef86cc00d
johndoeadams <johndoeadams@gmail.com>
2019-01-08 18:22:18 -0500

    more initial

---
+++ assets/js/fb.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDDFI02BH-xPlcQLthj4B62xmwN_VKBQok",
+  authDomain: "project1-5b839.firebaseapp.com",
+  databaseURL: "https://project1-5b839.firebaseio.com",
+  projectId: "project1-5b839",
+  storageBucket: "project1-5b839.appspot.com",
+  messagingSenderId: "616696108809"
+};
+firebase.initializeApp(config);
