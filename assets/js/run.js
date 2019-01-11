84ea85016184c261304b08dfe93ea1222acbdb52
johndoeadams <johndoeadams@gmail.com>
2019-01-10 20:08:56 -0500

    update js

--- assets/js/run.js
+++ assets/js/run.js
@@ -1,7 +1,33 @@
 // vars
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDDFI02BH-xPlcQLthj4B62xmwN_VKBQok",
+  authDomain: "project1-5b839.firebaseapp.com",
+  databaseURL: "https://project1-5b839.firebaseio.com",
+  projectId: "project1-5b839",
+  storageBucket: "project1-5b839.appspot.com",
+  messagingSenderId: "616696108809"
+};
+firebase.initializeApp(config);
 
 //objects
 
 //functions
 
 //ajax
+
+$("#search").click(function(){
+
+  function buildUrl(){
+    //collect from input fields
+
+    //create a local variable that holds the URL and adds on the params to the end
+  }
+
+  // create AJAX 'GET' call to collect articles
+
+  // Create a 'message' variable that is the text content of the email
+
+  // create seperate AJAX 'POST' to send email
+
+});
