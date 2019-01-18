df72872eeed84cec508c29f9ef77a4e2d8dcd048
Pranay <somethol@gmail.com>
2019-01-17 20:31:28 -0500

    Nutritionix updating

--- assets/js/run.js
+++ assets/js/run.js
@@ -27,34 +27,17 @@ var queryURL = "https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/
 }
 
 
-function buildQuery(amount, size, food) {
-  return amount + "%20" + size + "%20" + food;
-}
-
-function getBeverageInfo() {
-  $.ajax({
-    method: "GET",
-    url: "https://trackapi.nutritionix.com/v2/search/instant?query=1%20red%20bull&detailed=true&claims=true",
-    headers: {
-      'content-type': 'application/json',
-      'x-app-id' : 'd0025a85',
-      'x-app-key' : '423e3a386b4a6b7c4ad3eebfea0fa4b9',
-      'x-remote-user-id' : '0'
-    }
-  }).done(function(response) {
-    console.log(response);
-  }).fail(function(err){
-    //Log Error
-    console.log("Error: " + err);
-  });
-}
+var nutritionInfo;
 
 function getNutritionInfo() {
+  var queryString = beverage;
+  var amount = 1;
+
   $.ajax({
     method: "POST",
     url: "https://trackapi.nutritionix.com/v2/natural/nutrients",
     crossDomain : true,
-    data : '{"query":"20 fl oz bottles of sprite", "num_servings": 1, "locale":"en_US"}',
+    data : `{"query":"${queryString}", "num_servings":"${amount}", "locale":"en_US"}`,
     headers : {
       'content-type': 'application/json',
       'x-app-id' : 'd0025a85',
@@ -63,10 +46,28 @@ function getNutritionInfo() {
     }
   }).done(function(response) {
     console.log(response);
+    nutritionInfo = response.foods;
   }).fail(function(err){
     //Log Error
     console.log("Error: " + err);
   });
 }
-getBeverageInfo();
-getNutritionInfo();
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems, {});
+});
+
+var firstName = $('#first_name').val();
+var lastName = $('#last_name').val();
+var email = $('#email').val();
+var beverage = $('#beverage_menu option:selected').val();
+
+$(document).on('change', function() {
+  firstName = $('#first_name').val();
+  lastName = $('#last_name').val();
+  email = $('#email').val();
+  beverage = $('#beverage_menu option:selected').text();
+
+  getNutritionInfo(); 
+});
